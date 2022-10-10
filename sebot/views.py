58e10755a4f13200063d986cce427dce868cc304
Chad Whitacre <chadwhitacre@sentry.io>
2022-10-10 13:32:55 -0400

    Resolve CSRF failure 🤞

--- sebot/views.py
+++ sebot/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def homepage(request):
     return HttpResponse('Greetings, program!')
 
+@csrf_exempt
 def webhook(request):
     return HttpResponse('Greetings, GitHub!')
