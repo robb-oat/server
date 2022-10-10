92b9d1e3d6f583c6b519b3605c2a4cb77118005f
Chad Whitacre <chadwhitacre@sentry.io>
2022-10-10 15:21:08 -0400

    More dev setup, install OpenAI(!)

--- robboat/views.py
+++ robboat/views.py
@@ -1,9 +1,15 @@
+import os
+
+import openai
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
+openai.organization = os.environ['OPENAI_ORG_ID']
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 def homepage(request):
     return HttpResponse('Greetings, program! 💃')
 
 @csrf_exempt
 def webhook(request):
-    return HttpResponse('Greetings, GitHub!')
+    return HttpResponse(f'Greetings, GitHub!<br><br>OpenAI Org ID is {openai.organization}')
