fed098e7d480b6c381d65c190a206262f9a49d7e
Chad Whitacre <chadwhitacre@sentry.io>
2022-10-10 15:49:55 -0400

    Start parsing issue body

--- robboat/views.py
+++ robboat/views.py
@@ -1,7 +1,8 @@
+import json
 import os
 
 import openai
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
 openai.organization = os.environ['OPENAI_ORG_ID']
@@ -12,4 +13,20 @@ def homepage(request):
 
 @csrf_exempt
 def webhook(request):
-    return HttpResponse(f'Greetings, GitHub!<br><br>OpenAI Org ID is {openai.organization}')
+    data = json.loads(request.body)
+    body = data['issue']['body'] or ''  # body can be None
+    lines = body.splitlines()
+    if not lines:
+        return HttpResponseBadRequest('Empty issue')
+    filespec = lines[0]
+    instruction = ''
+    if len(lines) > 2:
+        instruction = lines[2:]
+    else:
+        return HttpResponseBadRequest('What is your instruction?')
+    return HttpResponse(f'''
+    Greetings, GitHub!<br><br>
+    OpenAI Org ID is {openai.organization}<br><br>
+    filespec: {filespec}<br><br>
+    instruction: {instruction}
+    ''')
