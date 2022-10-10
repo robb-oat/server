4c1fdd3db283532bfe80f325cd41a0346b2c19b2
Chad Whitacre <chadwhitacre@sentry.io>
2022-10-10 16:06:18 -0400

    Do filespec last

--- robboat/views.py
+++ robboat/views.py
@@ -13,17 +13,21 @@ def homepage(request):
 
 @csrf_exempt
 def webhook(request):
-    data = json.loads(request.body)
+    try:
+        data = json.loads(request.body)
+    except:
+        return HttpResponseBadRequest('Failed to parse request body as JSON')
+    if 'issue' not in data:
+        return HttpResponseBadRequest('Malformed POST body - no issue')
+    if 'body' not in data['issue']:
+        return HttpResponseBadRequest('Malformed POST body - no issue body')
     body = data['issue']['body'] or ''  # body can be None
     lines = body.splitlines()
-    if not lines:
-        return HttpResponseBadRequest('Empty issue')
-    filespec = lines[0]
-    instruction = ''
-    if len(lines) > 2:
-        instruction = lines[2:]
-    else:
-        return HttpResponseBadRequest('What is your instruction?')
+    if len(lines) < 3:
+        return HttpResponseBadRequest('Too few lines')
+    filespec = lines.pop()
+    lines.pop()
+    instruction = '\n'.join(lines)
     return HttpResponse(f'''
     Greetings, GitHub!<br><br>
     OpenAI Org ID is {openai.organization}<br><br>
