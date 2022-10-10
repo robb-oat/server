8bb813b3240994b1e596341c573f41f312fbb511
Chad Whitacre <chadwhitacre@sentry.io>
2022-10-10 13:53:58 -0400

    Working on CSRF bug - is latest getting deployed?

--- sebot/views.py
+++ sebot/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 def homepage(request):
-    return HttpResponse('Greetings, program!')
+    return HttpResponse('Greetings, program! 💃')
 
 @csrf_exempt
 def webhook(request):
