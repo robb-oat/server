0f11fb132573159fba6eda70a6a803a4a2d409fd
Chad Whitacre <chadwhitacre@sentry.io>
2022-10-13 22:14:58 -0400

    Update robboat/views.py

--- robboat/views.py
+++ robboat/views.py
@@ -25,7 +25,7 @@ github_app = GithubApp(
   private_key=github_app_private_key,
 )
 
-filespec_re = re.compile(r'https://github.com/[^/]+/[^/]+/blob/(?P<sha>[^/]+)/(?P<filepath>[^#]+)#L(?P<start>\d+)(?:-L(?P<end>\d*))?')
+filespec_re = re.compile(r'https://github.com/[^/]+/[^/]+/blob/(?P<sha>[^/]+)/(?P<filepath>[^#]+)#L(?P<start>\d+)(?:-L(?P<end>\d+))?')
 
 
 def homepage(request):
