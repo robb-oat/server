a754fb57335c3c56eff8daaed1f9fdf285f2b213
Chad Whitacre <chadwhitacre@sentry.io>
2022-12-28 16:51:58 -0500

    Refactor to break up webhook into handlers

--- robboat/views.py
+++ robboat/views.py
@@ -43,20 +43,57 @@ def error(request):
 @csrf_exempt
 def webhook(request):
 
-    try:    event_type = request.headers['X-GitHub-Event']
-    except: return HttpResponseBadRequest('Failed to identify event type')
-    try:    event = json.loads(request.body)
-    except: return HttpResponseBadRequest('Failed to parse request body as JSON')
-    try:    installation_id = event['installation']['id']
-    except: return HttpResponseBadRequest('Failed to identify installation')
-    try:    org_repo = event['repository']['full_name']
-    except: return HttpResponseBadRequest('Failed to identify repository')
-    try:    event_subtype= event['action']
-    except: return HttpResponseBadRequest('Failed to identify event subtype')
-    if (event_type, event_subtype) not in (('issues', 'opened'), ('issues', 'edited')):
-        return JsonResponse({
-            'ignored': f'{event_type}.{event_subtype}'
-        })
+    # Parse event
+    # ===========
+
+    try:
+        event = json.loads(request.body)
+    except:
+        return HttpResponseBadRequest('Failed to parse request body as JSON')
+
+    try:
+        event_type = request.headers['X-GitHub-Event']
+    except:
+        return HttpResponseBadRequest('Failed to identify event type')
+    else:
+        event['X-GitHub-Event'] = event_type  # Kludge. Why is this not already in here somewhere?
+
+    try:
+        event_subtype= event['action']
+    except:
+        return HttpResponseBadRequest('Failed to identify event subtype')
+
+
+    # Parse repo
+    # ==========
+
+    try:
+        installation_id = event['installation']['id']
+    except:
+        return HttpResponseBadRequest('Failed to identify installation')
+
+    try:
+        org_repo = event['repository']['full_name']
+    except:
+        return HttpResponseBadRequest('Failed to identify repository')
+
+    repo = github_app.installation_client(installation_id).get_repo(org_repo)
+
+
+    # Hand off
+    # ========
+
+    func = globals().get(f'handle_{event_type}_{event_subtype}', handle_ignored)
+    return func(repo, event)
+
+
+def handle_ignored(repo, event):
+    return JsonResponse({
+        'ignored': f"{event['X-GitHub-Event']}.{event['action']}"
+    })
+
+
+def handle_issues_opened(repo, event):
     if 'issue' not in event:
         return HttpResponseBadRequest('Malformed POST body - no issue')
     if 'number' not in event['issue']:
@@ -79,7 +116,7 @@ def webhook(request):
         start, end = map(int, (start, maybe_end))
     instruction = '\n'.join(all_lines[2:])
 
-    content_url = f'https://raw.githubusercontent.com/{org_repo}/{sha}/{filepath}'
+    content_url = f'https://raw.githubusercontent.com/{repo.full_name}/{sha}/{filepath}'
     content_lines = httpx.get(content_url).text.splitlines()
     before = '\n'.join(content_lines[:start-1])
     old_passage = '\n'.join(content_lines[start-1:end])
@@ -87,8 +124,6 @@ def webhook(request):
     answer = openai.Edit.create(model='code-davinci-edit-001', input=old_passage, instruction=instruction)
     new_passage = answer['choices'][0]['text']
     new_content = before + new_passage + after
-
-    repo = github_app.installation_client(installation_id).get_repo(org_repo)
 
     branch = f'issue-{issue_number}'
     old_commit = repo.get_git_commit(sha)
@@ -109,3 +144,5 @@ def webhook(request):
         'filespec': filespec.groups(),
         'instruction': instruction,
     })
+
+handle_issues_edited = handle_issues_opened
