a094b43c070335658380d29077da27134441f4de
Chad Whitacre <chadwhitacre@sentry.io>
2022-12-29 16:14:42 -0500

    Add comment replies, safely
    - avoid spamming all PRs
    - avoid spamming our own PRs
    - don't bother protecting against unlikely GitHub API weirdness (we have
      Sentry installed now)
    - handle single-line case in PR review response
    - post a reply to review comments

--- robboat/views.py
+++ robboat/views.py
@@ -24,6 +24,7 @@ github_app = GithubApp(
   app_id=int(os.environ['GITHUB_APP_ID']),
   private_key=github_app_private_key,
 )
+BOT_USER_ID = int(os.environ['GITHUB_BOT_USER_ID'])
 
 filespec_re = re.compile(r'https://github.com/[^/]+/[^/]+/blob/(?P<sha>[^/]+)/(?P<filepath>[^#]+)#L(?P<start>\d+)(?:-L(?P<end>\d+))?')
 
@@ -44,70 +45,37 @@ def error(request):
 def webhook(request):
 
     # Parse event
-    # ===========
-
-    try:
-        event = json.loads(request.body)
-    except:
-        return HttpResponseBadRequest('Failed to parse request body as JSON')
-
-    try:
-        event_type = request.headers['X-GitHub-Event']
-    except:
-        return HttpResponseBadRequest('Failed to identify event type')
-    else:
-        event['X-GitHub-Event'] = event_type  # Kludge. Why is this not already in here somewhere?
-
-    try:
-        event_subtype= event['action']
-    except:
-        return HttpResponseBadRequest('Failed to identify event subtype')
-
+    event = json.loads(request.body)
+    event_type = request.headers['X-GitHub-Event']
+    event['X-GitHub-Event'] = event_type  # Kludge. Why is this not already in here somewhere?
+    event_subtype= event['action']
 
     # Parse repo
-    # ==========
-
-    try:
-        installation_id = event['installation']['id']
-    except:
-        return HttpResponseBadRequest('Failed to identify installation')
-
-    try:
-        org_repo = event['repository']['full_name']
-    except:
-        return HttpResponseBadRequest('Failed to identify repository')
-
+    installation_id = event['installation']['id']
+    org_repo = event['repository']['full_name']
     repo = github_app.installation_client(installation_id).get_repo(org_repo)
 
-
     # Hand off
-    # ========
-
-    func = globals().get(f'handle_{event_type}_{event_subtype}', handle_ignored)
+    func = globals().get(f'handle_{event_type}_{event_subtype}', ignore)
     return func(repo, event)
 
 
-def handle_ignored(repo, event):
+def ignore(repo, event, reason='uninteresting event'):
     return JsonResponse({
-        'ignored': f"{event['X-GitHub-Event']}.{event['action']}"
+        'ignored': f"{event['X-GitHub-Event']}.{event['action']}",
+        'reason': reason,
     })
 
 
 def handle_issues_opened(repo, event):
-    if 'issue' not in event:
-        return HttpResponseBadRequest('Malformed POST body - no issue')
-    if 'number' not in event['issue']:
-        return HttpResponseBadRequest('Malformed POST body - no issue number')
-    if 'body' not in event['issue']:
-        return HttpResponseBadRequest('Malformed POST body - no issue body')
     body = event['issue']['body'] or ''  # body can be None
     issue_number = event['issue']['number']
     all_lines = body.splitlines()
     if len(all_lines) < 3:
-        return HttpResponseBadRequest('Too few lines')
+        return ignore(repo, event, 'too few lines')
     filespec = filespec_re.match(all_lines[0])
     if filespec is None:
-        return HttpResponseBadRequest('Malformed filespec')
+        return ignore(repo, event, 'malformed filespec')
     sha, filepath, start, maybe_end = filespec.groups()
     if maybe_end is None:
         start = int(start)
@@ -119,6 +87,7 @@ def handle_issues_opened(repo, event):
 
     new_commit, response = edit(repo, branch, sha, filepath, start, end, instruction)
 
+    # Open a new pull request.
     repo.create_git_ref(f'refs/heads/{branch}', new_commit.sha)
     repo.create_pull(
         title=f'Address #{issue_number}',
@@ -132,17 +101,32 @@ handle_issues_edited = handle_issues_opened
 
 
 def handle_pull_request_review_comment_created(repo, event):
+    if event['pull_request']['user']['id'] != BOT_USER_ID:
+        # It would be really no good if we responded to every pull request
+        # comment in a busy repo. I shudder at the thought.
+        return ignore(repo, event, 'not our PR')
+
     comment = event['comment']
+    if comment['user']['id'] == BOT_USER_ID:
+        # Less bad (since GitHub has some protection built-in for this) but
+        # still bad if we respond recursively to our own comments.
+        return ignore(repo, event, 'avoiding recursion')
+
     instruction = comment['body']
     filepath = comment['path']
     sha = comment['commit_id'] # github.com/orgs/community/discussions/24449 tho
-    start = int(comment['start_line'])
+    maybe_start = comment['start_line']
     end = int(comment['line'])
+    start = maybe_start if maybe_start is not None else end
     branch = event['pull_request']['head']['ref']
 
     new_commit, response = edit(repo, branch, sha, filepath, start, end, instruction)
 
+    # Update the PR and reply in the comments.
     repo.get_git_ref(f'heads/{branch}').edit(new_commit.sha)
+    pr = repo.get_pull(event['pull_request']['number'])
+    pr.create_review_comment_reply(comment['id'], f'Done in {new_commit.sha}.')
+
     return response
 
 
